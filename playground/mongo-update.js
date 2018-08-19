2cda3dff67455939c295d0d5a1ceb254f29b50f7
Rishi Kolasani <rkolasani95@gmail.com>
2018-08-19 12:16:01 -0400

    update commit

---
+++ playground/mongo-update.js
@@ -0,0 +1,24 @@
+//var MongoClient=require('mongodb').MongoClient;
+const {MongoClient,ObjectId}=require('mongodb');// we can use the objectify declaration as well
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('something went wrong');
+  }
+
+db.collection('Todo').findOneAndUpdate({_id:new ObjectId("5b79961633c9e10e94add795")},{
+  $set:{
+    completed:true
+  }
+},{
+  returnOriginal:false
+}
+
+).then((result)=>{
+  console.log(result);
+
+
+});
+
+
+  //  db.close();
+}  );
